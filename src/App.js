f814de36695e8580bfb4cef8b82158a26fd8b026
Sheyla <u201619750@upc.edu.pe>
2021-11-26 21:16:04 -0500

    custom

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 import awsExports from "./aws-exports";
 import RouterPage from "./pages/RouterPage";
 
+import { AmplifySignUp } from "aws-amplify";
+import { AmplifySignIn } from "aws-amplify";
 Amplify.configure(awsExports);
 
 class Header extends Component {
@@ -30,5 +32,32 @@ class App extends Component {
     );
   }
 }
+
+<AmplifyAuthenticator usernameAlias="email">
+  <AmplifySignUp
+    slot="sign-up"
+    usernameAlias="email"
+    headerText="My Custom Sign Up Text!"
+    formFields={[
+      {
+        type: "email",
+        label: "Custom Email Label",
+        placeholder: "Custom email placeholder",
+        inputProps: { required: true, autocomplete: "username" },
+      },
+      {
+        type: "password",
+        label: "Custom Password Label",
+        placeholder: "Custom password placeholder",
+        inputProps: { required: true, autocomplete: "new-password" },
+      },
+    ]}
+  />
+  <AmplifySignIn
+    slot="sign-in"
+    usernameAlias="email"
+    headerText="My Custom Sign In Text"
+  />
+</AmplifyAuthenticator>;
 
 export default App;
